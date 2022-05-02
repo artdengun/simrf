04606ee6e5e7dd69c445e69836ac9a51fdf8a683
Deni Gunawan <artdengun@gmail.com>
2022-05-03 00:25:15 +0700

    export pdf

--- src/main/java/com/deni/gunawan/sistemmanajemenricheesefactory/controllers/reports/export/ExportPdfController.java
+++ src/main/java/com/deni/gunawan/sistemmanajemenricheesefactory/controllers/reports/export/ExportPdfController.java
@@ -1,19 +1,14 @@
 package com.deni.gunawan.sistemmanajemenricheesefactory.controllers.reports.export;
 
-import com.deni.gunawan.sistemmanajemenricheesefactory.config.reporting.pdf.AssetPdfReporting;
-import com.deni.gunawan.sistemmanajemenricheesefactory.config.reporting.pdf.TransferplantPdfReporting;
-import com.deni.gunawan.sistemmanajemenricheesefactory.services.PDFService;
+import com.deni.gunawan.sistemmanajemenricheesefactory.config.reporting.pdf.PDFReporting;
 import lombok.AllArgsConstructor;
-import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperPrint;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 
 /**
  *
@@ -24,59 +19,56 @@ import java.util.Date;
 @AllArgsConstructor
 public class ExportPdfController {
 
-        private PDFService pdfService;
+        private PDFReporting pdfReporting;
+        private HttpServletResponse httpServletResponse;
 
-    @GetMapping(value = "/transferplant/export/pdf")
-    public void exportTransferplantPDF(HttpServletResponse response) throws IOException, JRException {
-        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
-        String currentDateTime = dateFormatter.format(new Date());
-        response.setContentType("application/x-download");
-        response.setHeader("Content-Disposition", String.format("attachment; filename=\"data_transferplant_"+currentDateTime+".pdf\""));
-        OutputStream outputStream = response.getOutputStream();
-        pdfService.exportReportTransferPlant(outputStream);
+        @GetMapping(value = "/asset/export/pdf")
+        public void getPDFAssetReport() throws Exception {
+            httpServletResponse.setContentType("application/pdf");
+            httpServletResponse.setHeader("Content-Disposition", "attachment; filename=\"data_asset.pdf\"");
+            JasperPrint jasperPrint = pdfReporting.generateAssetReporting();
+            JasperExportManager.exportReportToPdfStream(jasperPrint,httpServletResponse.getOutputStream());
+
+        }
+
+        @GetMapping(value = "/frozen/export/pdf")
+        public void getPDFFrozenReport() throws Exception {
+        httpServletResponse.setContentType("application/pdf");
+        httpServletResponse.setHeader("Content-Disposition", "attachment; filename=\"data_frozen.pdf\"");
+        JasperPrint jasperPrint = pdfReporting.generateFrozenReporting();
+        JasperExportManager.exportReportToPdfStream(jasperPrint,httpServletResponse.getOutputStream());
+
     }
 
 
-    @GetMapping(value = "/asset/export/pdf")
-    public void exportAssetPDF(HttpServletResponse response) throws IOException, JRException {
-        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
-        String currentDateTime = dateFormatter.format(new Date());
-        response.setContentType("application/x-download");
-        response.setHeader("Content-Disposition", String.format("attachment; filename=\"data_asset_"+currentDateTime+".pdf\""));
-        OutputStream outputStream = response.getOutputStream();
-        pdfService.exportReportAsset(outputStream);
+        @GetMapping(value = "/raw/export/pdf")
+        public void getPDFRawReport() throws Exception {
+        httpServletResponse.setContentType("application/pdf");
+        httpServletResponse.setHeader("Content-Disposition", "attachment; filename=\"data_raw.pdf\"");
+        JasperPrint jasperPrint = pdfReporting.generateRawReporting();
+        JasperExportManager.exportReportToPdfStream(jasperPrint,httpServletResponse.getOutputStream());
+
     }
-
-
-    @GetMapping(value = "/frozen/export/pdf")
-    public void exportFrozenPDF(HttpServletResponse response) throws IOException, JRException {
-        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
-        String currentDateTime = dateFormatter.format(new Date());
-        response.setContentType("application/x-download");
-        response.setHeader("Content-Disposition", String.format("attachment; filename=\"data_frozen_"+currentDateTime+".pdf\""));
-        OutputStream outputStream = response.getOutputStream();
-        pdfService.exportReportFrozen(outputStream);
-    }
-
 
     @GetMapping(value = "/retur/export/pdf")
-    public void exportReturPDF(HttpServletResponse response) throws IOException, JRException {
-        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
-        String currentDateTime = dateFormatter.format(new Date());
-        response.setContentType("application/x-download");
-        response.setHeader("Content-Disposition", String.format("attachment; filename=\"data_retur_"+currentDateTime+".pdf\""));
-        OutputStream outputStream = response.getOutputStream();
-        pdfService.exportReportRetur(outputStream);
+    public void getPDFReturReport() throws Exception {
+        httpServletResponse.setContentType("application/pdf");
+        httpServletResponse.setHeader("Content-Disposition", "attachment; filename=\"data_retur.pdf\"");
+        JasperPrint jasperPrint = pdfReporting.generateReturReporting();
+        JasperExportManager.exportReportToPdfStream(jasperPrint,httpServletResponse.getOutputStream());
+
     }
 
-    @GetMapping(value = "/raw/export/pdf")
-    public void exportRawPDF(HttpServletResponse response) throws IOException, JRException {
-        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
-        String currentDateTime = dateFormatter.format(new Date());
-        response.setContentType("application/x-download");
-        response.setHeader("Content-Disposition", String.format("attachment; filename=\"data_raw_"+currentDateTime+".pdf\""));
-        OutputStream outputStream = response.getOutputStream();
-        pdfService.exportReportRaw(outputStream);
+
+    @GetMapping(value = "/transferplant/export/pdf")
+    public void getPDFTransferPlantReport() throws Exception {
+        httpServletResponse.setContentType("application/pdf");
+        httpServletResponse.setHeader("Content-Disposition", "attachment; filename=\"data_transferplant.pdf\"");
+        JasperPrint jasperPrint = pdfReporting.generateTransferPlantReporting();
+        JasperExportManager.exportReportToPdfStream(jasperPrint,httpServletResponse.getOutputStream());
+
     }
+
+
 
 }
